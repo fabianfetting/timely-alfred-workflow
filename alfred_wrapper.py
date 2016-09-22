4ddfd2bd2f37becc78c45c6c07664d44fec5ccde
Fabian Fetting <mail@fabianfetting.de>
2016-09-22 20:44:25 +0200

    Fix script to handle alfred workflow inputs

---
+++ alfred_wrapper.py
@@ -0,0 +1,51 @@
+import subprocess
+import sys
+import json
+
+
+def print_time(t):
+    output = {
+        'items': [
+            {
+                'uid': 'result',
+                'type': 'file',
+                'title': t,
+                'subtitle': sys.argv[1],
+                'arg': sys.argv[1],
+                'icon': {
+                    'path': 'icon.png'
+                }
+            }
+        ]
+    }
+
+    output_json = json.dumps(output)
+    sys.stdout.write(output_json)
+
+
+def print_invalid():
+    output = {
+        'items': [
+            {
+                'uid': 'invalid',
+                'type': 'file',
+                'title': 'Invalid',
+                'subtitle': sys.argv[1],
+                'arg': sys.argv[1],
+                'icon': {
+                    'path': 'icon.png'
+                }
+            }
+        ]
+    }
+    output_json = json.dumps(output)
+    sys.stdout.write(output_json)
+
+
+try:
+    result = subprocess.check_output(['./timely.py'] + sys.argv[1:])
+except subprocess.CalledProcessError as e:
+    print_invalid()
+    exit(0)
+
+print_time(result[:-1])
--- timely.py
+++ timely.py
@@ -5,11 +5,14 @@ import sys
 import operator
 import re
 
+
 def print_time(t):
     total_seconds = int(abs(t).total_seconds())
     hours, remainder = divmod(total_seconds, 60*60)
     minutes, seconds = divmod(remainder, 60)
-    print(t.days < 0 and '-' or '+', '{}h {}m {}s'.format(hours, minutes, seconds))
+    sign = t.days < 0 and '- ' or ''
+    print(sign + '{}h {}m {}s'.format(hours, minutes, seconds))
+
 
 def to_delta(op, time):
     time_format = ''
@@ -17,15 +20,17 @@ def to_delta(op, time):
     time_format += 'm' in time and '%Mm' or ''
     time_format += 's' in time and '%Ss' or ''
 
-    try: date_time = datetime.strptime(time, time_format)
+    try:
+        date_time = datetime.strptime(time, time_format)
     except ValueError:
         raise Exception('Invalid input')
         exit(1)
     time_delta = timedelta(hours=date_time.hour, minutes=date_time.minute, seconds=date_time.second)
     return op == '-' and -time_delta or +time_delta
 
-calc_string = reduce(operator.concat, sys.argv[1:])
-splitted_string = re.split(r'([+,-])', calc_string)
+
+input_string = reduce(operator.concat, sys.argv[1:]).replace(' ', '')
+splitted_string = re.split(r'([+,-])', input_string)
 time_strings = splitted_string[::2]
 time_ops = ['+'] + splitted_string[1::2]
 grouped_times = zip(time_ops, time_strings)
